efb865dfcf9a2067c29828d235e098dc4e8fffe2
Kaela <kaelaagreen@gmail.com>
2021-06-10 18:47:49 -0400

    last push! thanks class :)

--- script.js
+++ script.js
@@ -14,21 +14,18 @@ console.log ("Standby")
 
 }
 
-// var tempCelcius = 30
-// var targetUnit = "F"
+var tempCelcius = 30
+var targetUnit = "F"
 
-// switch(tempCelcius){
-//     case "F":
-//             (tempCelcius*1.8)+32;
-//             console.log(tempCelcius + " is " +  + " in Farenheit");
-//         }
-//         break;
-// //     case "K":
-// //        function convertedTemp = tempCelcius+273.15;
-// //         console.log(tempCelcius + " is " + convertedTemp + " in Kelvin");
-// //         break;
-// //     case "C":
-// //         function console.log("It's already in Celcius.");
-// //         break;      
-// // }
+switch(targetUnit){
+    case "F":
+        console.log(tempCelcius + " is " + (32+(tempCelcius*1.8)) + " in Farenheit");
+        break;
+    case "K":
+        console.log(tempCelcius + " is " + (tempCelcius+273.15) + " in Kelvin");
+        break;
+    case "C":
+        console.log("It's already in Celcius.");
+        break;      
+}
 
