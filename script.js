8c4fb09218317653335cd1b03b15926e6c0dbad5
Kaela <kaelaagreen@gmail.com>
2021-06-10 18:22:49 -0400

    cond1 2nd push. 1st part finished extended still in progress. commented out for viewing.

---
+++ script.js
@@ -0,0 +1,34 @@
+let desiredTemp = "67"
+let actualTemp = "90"
+
+if (actualTemp > desiredTemp){
+console.log("Run A/C")
+
+} else if (actualTemp < desiredTemp) {
+
+console.log("Run heat")
+
+} else if (actualTemp == desiredTemp){
+
+console.log ("Standby")
+
+}
+
+// var tempCelcius = 30
+// var targetUnit = "F"
+
+// switch(tempCelcius){
+//     case "F":
+//             (tempCelcius*1.8)+32;
+//             console.log(tempCelcius + " is " +  + " in Farenheit");
+//         }
+//         break;
+// //     case "K":
+// //        function convertedTemp = tempCelcius+273.15;
+// //         console.log(tempCelcius + " is " + convertedTemp + " in Kelvin");
+// //         break;
+// //     case "C":
+// //         function console.log("It's already in Celcius.");
+// //         break;      
+// // }
+
